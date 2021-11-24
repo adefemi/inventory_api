8a6d7583a7652d4406e496f9e1aa8a45b9d6e9ee
Adefemi <adefemigreat1995@gmail.com>
2021-11-24 14:23:56 +0100

    fix: correct object name

--- inventory_api/custom_methods.py
+++ inventory_api/custom_methods.py
@@ -8,7 +8,7 @@ class IsAuthenticatedCustom(BasePermission):
 
     def has_permission(self, request, _):
         try:
-            auth_token = request.Meta.get("HTTP_AUTHORIZATION", None)
+            auth_token = request.META.get("HTTP_AUTHORIZATION", None)
         except Exception:
             return False
         if not auth_token:
