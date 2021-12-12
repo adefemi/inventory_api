42c79d475938ff66d220fc017f2ea76e3aafec4e
Adefemi <adefemigreat1995@gmail.com>
2021-12-12 13:02:02 +0100

    fix group serializer

--- app_control/serializers.py
+++ app_control/serializers.py
@@ -8,7 +8,7 @@ class InventoryGroupSerializer(serializers.ModelSerializer):
     created_by = CustomUserSerializer(read_only=True)
     created_by_id = serializers.CharField(write_only=True, required=False)
     belongs_to = serializers.SerializerMethodField(read_only=True)
-    belongs_to_id = serializers.CharField(write_only=True)
+    belongs_to_id = serializers.CharField(write_only=True, required=False)
     total_items = serializers.CharField(read_only=True, required=False)
 
     class Meta:
