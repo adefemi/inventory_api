dcb45fc4f46ae2417903304c75c718919bb4d099
Adefemi <adefemigreat1995@gmail.com>
2022-01-03 19:56:57 +0100

    updated

--- app_control/views.py
+++ app_control/views.py
@@ -94,10 +94,10 @@ class ShopView(ModelViewSet):
             return self.queryset
 
         data = self.request.query_params.dict()
-        data.pop("page")
+        data.pop("page", None)
         keyword = data.pop("keyword", None)
 
-        results = self.queryset(**data)
+        results = self.queryset.filter(**data)
 
         if keyword:
             search_fields = (
